5dbd59f33e5b674a47f24e60a5c363c98a8bcd1f
unknown <tommyconner96@gmail.com>
2019-12-13 19:56:37 -0500

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function headerFunction() {
+    //created elements
+    let header = document.createElement('div')
+    let date = document.createElement('span')
+    let h1 = document.createElement('h1')
+    let temp = document.createElement('span')
+    //classes
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+    //appended children
+    header.appendChild(date)
+    header.appendChild(h1)
+    header.appendChild(temp)
+    //text
+    date.textContent = 'SMARCH 28, 2019'
+    h1.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+    return header
+}
+
+let contents = document.querySelector('.header-container')
+contents.prepend(headerFunction())
+// console.log(headerFunction()) //doesn't work? but it works on the site. Moving on.
